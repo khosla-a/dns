562b60b77418a870da17bdfacccad524265dc4ad
khosla-a <143970960+khosla-a@users.noreply.github.com>
2023-10-28 17:46:32 -0500

    8
    8

--- DNSServer.py
+++ DNSServer.py
@@ -102,7 +102,7 @@ dns_records = {
         dns.rdatatype.CNAME: 'www.nyu.edu',
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com')],
         dns.rdatatype.NS: 'ns1.nyu.edu', 
-        dns.rdatatype.TXT: (str(encrypted_value)),
+        dns.rdatatype.TXT: ("hello"),
     },
 
    
